9b427982761c36887fa730311be138c1829457a0
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2017-10-31 14:41:16 +0100

    Fix play.sh folder

--- play.sh
+++ play.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is meant to be invoked in a cronjob to continously run our infra playbooks
-cd /home/centos/network-infra
+cd /home/centos/network-infra-playbooks
 git checkout master -q
 git pull -q
 flock -n /tmp/ansible-playbook.lock ansible-playbook --vault-password-file .vaultpass -i inventories/$(cat .environment)/hosts site.yml
